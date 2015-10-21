bbbc5658225701ac59aeff6cdbd25aa1c8c6c46e
minigt <minigtraverso@gmail.com>
2015-10-20 22:39:13 -0300

    Main program and MPU functions

--- Software/src/MPU_6050.c
+++ Software/src/MPU_6050.c
@@ -2,26 +2,28 @@
 #include "../../Rescatar/inc/MPU_6050.h"
 
 #include "ciaak.h"            /* <= ciaa kernel header */
-#include <math.h>
+#include <math.h>			//para arcotg en filtro
 
-#define grav -9.81
-#define peso 0.95
-#define radagra 57.3   // 180/pi
-#define phi 3.141592
+/*==================[macros]=================================================*/
+
+#define GRAV 		-9.81
+#define PESO 		0.95
+#define RADAGRA 	57.3   			// 180/pi
+#define PHI 		3.141592
+
+/*==================[internal data declaration]==============================*/
+
+/*en esta estructura guardo los offset luego de la calibracion*/
+static MPU_data_struct MPU_data_offset;
+/*dt almacena el delta tiempo (en ms) segun el cual se toman las muestras*/
+static float dt;
+/* en esta estructura guardo las lecturas del MPU*/
+static MPU_data_struct MPU_data;
 
 
-int16_t x_gyro_offset,y_gyro_offset,z_gyro_offset,x_accel_offset,y_accel_offset,z_accel_offset;
-uint8_t regAdress,data_byte;
-int16_t data_word;
-float dt;
-static float xgyro=0,ygyro=0,zgyro=0,xacc=0,yacc=0,zacc=0;
 
-int32_t  MPU_6050_open(uint32_t  clockRate){
-	int32_t	fd_i2c = ciaaPOSIX_open("/dev/i2c/0", O_RDWR);//Abrimos el file descriptor para i2c (I2C0)
-	ciaaPOSIX_ioctl(fd_i2c, ciaaPOSIX_IOCTL_SET_CLOCKRATE, (void *)clockRate);//Configuramos el clockrate
-	ciaaPOSIX_ioctl(fd_i2c, ciaaPOSIX_IOCTL_SET_SLAVEADD, (void *)MPU6050_ADDRESS);//Configuramos la direccion del esclavo
-	return fd_i2c;
-}
+/*==================[external functions definition]==========================*/
+
 
 uint8_t  MPU_6050_readReg(int32_t	fd_i2c,uint8_t RegAddress,uint8_t *data){
 	ciaaPOSIX_write(fd_i2c,&RegAddress,1);/*
@@ -40,177 +42,189 @@ uint8_t  MPU_6050_writeReg(int32_t	fd_i2c,uint8_t RegAddress,uint8_t data){
 }
 
 uint8_t  MPU_6050_test(int32_t fd_i2c){
+	/*sirve para almacenar temporalmente los valores de los registros que contienen
+	 *datos de 8bits
+	 */
 	uint8_t data;
+
 	MPU_6050_readReg (fd_i2c,MPU6050_RA_WHO_AM_I,&data);
 	if(data!=0x68)
-		return (1==0);
-	MPU_6050_readReg (fd_i2c,MPU6050_RA_GYRO_CONFIG,&data);
-	if(data&&0b00011000!=0)
-		return (1==0);
-	return (1==1);
+		return (0);
+	return 1;
 }
 
 
+uint8_t  MPU_6050_update(int32_t	fd_i2c,MPU_data_struct *datos){
+	/*sirve para almacenar temporalmente los valores de los registros que contienen
+	 *datos de 16bits
+	 */
+	int16_t data_word;
 
-//	*reg=floorf(*reg * 10) / 10;
+	/*sirve para almacenar temporalmente los valores de los registros que contienen
+	 *datos de 8bits
+	 */
+	uint8_t data_byte;
 
-/*
- * filtro
- */
-//	switch (add){
-//	case MPU6050_RA_GYRO_XOUT_H:
-//		float pitchAcc = atan2f((float)accData[1], (float)accData[2]) * 180 / M_PI;
-//		*reg = *reg * 0.98 + pitchAcc * 0.02;
-//		break;
-//		//		case MPU6050_RA_GYRO_YOUT_H:data_word=(data_word | data_byte)+y_gyro_offset;break;
-//		//		case MPU6050_RA_GYRO_ZOUT_H:data_word=(data_word | data_byte)+z_gyro_offset;break;
-//	}
+	/*sirve para almacenar temporalmente las direcciones a pasar como parametro de los
+	 * registros que deberan ser leidos
+	 */
+	uint8_t regAdress;
 
-
-uint8_t  MPU_6050_readData(int32_t	fd_i2c,float *xg,float *yg,float *zg,float *xa,float *ya,float *za){
 	regAdress=MPU6050_RA_INT_STATUS;
 	MPU_6050_readReg(fd_i2c,regAdress,&data_byte);
 	if((data_byte & 0b00000001) ==1){
 		/*
-		 * accel registro x
+		 * lectura y escalamiento del registro x del acelerometro
 		 */
 		regAdress=MPU6050_RA_ACCEL_XOUT_H;
 		MPU_6050_readReg(fd_i2c,regAdress,&data_byte);
 		data_word=(int16_t)((data_byte<<8)&0xff00);
 		++regAdress;
 		MPU_6050_readReg(fd_i2c,regAdress,&data_byte);
-		data_word=((data_word | data_byte)+x_accel_offset);
-		*xa=((float)data_word)/(INT16_MAX/AFS_SEL);
-		*xa=(*xa)*grav;
+		data_word=((data_word | data_byte)+MPU_data_offset.xacc);
+		MPU_data.xacc=((float)data_word)/(INT16_MAX/AFS_SEL);
+		MPU_data.xacc=MPU_data.xacc*GRAV;
+
 		/*
-		 * accel registro y
+		 * lectura y escalamiento del registro y del acelerometro
 		 */
 		regAdress=MPU6050_RA_ACCEL_YOUT_H;
 		MPU_6050_readReg(fd_i2c,regAdress,&data_byte);
 		data_word=(int16_t)((data_byte<<8)&0xff00);
 		++regAdress;
 		MPU_6050_readReg(fd_i2c,regAdress,&data_byte);
-		data_word=((data_word | data_byte)+y_accel_offset);
-		*ya=((float)data_word)/(INT16_MAX/AFS_SEL);
-		*ya=(*ya)*grav;
+		data_word=((data_word | data_byte)+MPU_data_offset.yacc);
+		MPU_data.yacc=((float)data_word)/(INT16_MAX/AFS_SEL);
+		MPU_data.yacc=MPU_data.yacc*GRAV;
+
 		/*
-		 * accel registro z
+		 * lectura y escalamiento del registro z del acelerometro
 		 */
 		regAdress=MPU6050_RA_ACCEL_ZOUT_H;
 		MPU_6050_readReg(fd_i2c,regAdress,&data_byte);
 		data_word=(int16_t)((data_byte<<8)&0xff00);
 		++regAdress;
 		MPU_6050_readReg(fd_i2c,regAdress,&data_byte);
-		data_word=((data_word | data_byte)+z_accel_offset);
-		*za=((float)data_word)/(INT16_MAX/AFS_SEL);
-		*za=(*za)*grav;
+		data_word=((data_word | data_byte)+MPU_data_offset.zacc);
+		MPU_data.zacc=((float)data_word)/(INT16_MAX/AFS_SEL);
+		MPU_data.zacc=(MPU_data.zacc)*GRAV;
+
 		/*
-		 * gyro registro x
+		 * lectura del registro x del giroscopio
 		 */
 		regAdress=MPU6050_RA_GYRO_XOUT_H;
 		MPU_6050_readReg(fd_i2c,regAdress,&data_byte);
 		data_word=(int16_t)((data_byte<<8)&0xff00);
 		++regAdress;
 		MPU_6050_readReg(fd_i2c,regAdress,&data_byte);
-		data_word=(data_word | data_byte)+x_gyro_offset;
+		data_word=(data_word | data_byte)+MPU_data_offset.xgyro;
 
-		//filtro registro x
+		//filtro y escalamiento del registro x del giroscopio
 
-		*xg=peso*((((float)data_word/(INT16_MAX/FS_SEL)))*dt+*xg)+(1-peso)*atan(*ya/(sqrt(pow(*xa,2)+pow(*za,2))))*(float)radagra;
+		MPU_data.xgyro=PESO*((((float)data_word/(INT16_MAX/FS_SEL)))*dt+MPU_data.xgyro)+(1-PESO)*atan(MPU_data.yacc/(sqrt(pow(MPU_data.xacc,2)+pow(MPU_data.zacc,2))))*(float)RADAGRA;
 
 		/*
-		 * gyro registro y
+		 * lectura del registro y del giroscopio
 		 */
 		regAdress=MPU6050_RA_GYRO_YOUT_H;
 		MPU_6050_readReg(fd_i2c,regAdress,&data_byte);
 		data_word=(int16_t)((data_byte<<8)&0xff00);
 		++regAdress;
 		MPU_6050_readReg(fd_i2c,regAdress,&data_byte);
-		data_word=(data_word | data_byte)+y_gyro_offset;
+		data_word=(data_word | data_byte)+MPU_data_offset.ygyro;
 
-		//filtro registro y
-		*yg=peso*((((float)data_word/(INT16_MAX/FS_SEL)))*dt+*yg)-(1-peso)*atan(*xa/(sqrt(pow(*ya,2)+pow(*za,2))))*(float)radagra;
+		//filtro y escalamiento del registro y del giroscopio
+		MPU_data.ygyro=PESO*((((float)data_word/(INT16_MAX/FS_SEL)))*dt+MPU_data.ygyro)-(1-PESO)*atan(MPU_data.xacc/(sqrt(pow(MPU_data.yacc,2)+pow(MPU_data.zacc,2))))*(float)RADAGRA;
 
 		/*
-		 * gyro registro z
+		 * lectura y escalamiento del registro z del giroscopio
 		 */
 		regAdress=MPU6050_RA_GYRO_ZOUT_H;
 		MPU_6050_readReg(fd_i2c,regAdress,&data_byte);
 		data_word=(int16_t)((data_byte<<8)&0xff00);
 		++regAdress;
 		MPU_6050_readReg(fd_i2c,regAdress,&data_byte);
-		data_word=(data_word | data_byte)+z_gyro_offset;
-		*zg+=(((float)data_word/(INT16_MAX/FS_SEL)))*dt;
+		data_word=(data_word | data_byte)+MPU_data_offset.zgyro;
+		MPU_data.zgyro+=(((float)data_word/(INT16_MAX/FS_SEL)))*dt;
+
 		//filtro registro z debe ser implementado de otra forma
 
-		//*zg=peso*((((float)data_word/(INT16_MAX/FS_SEL)))*dt+*zg)+(1-peso)*atan2f(*xa,*ya)*(float)radagra;
-
-		/*
-		 * no se si son necesarias
-		 */
-		xgyro=*xg;
-		ygyro=*yg;
-		zgyro=*zg;
-		xacc=*xa;
-		yacc=*ya;
-		zacc=*za;
-		/*
-		 *
-		 */
-
+		*datos=MPU_data;
 		return 1;
 	}
 	return 0;
 }
 
 uint8_t  MPU_6050_calibrate(int32_t	fd_i2c){
-	char count=0;
-	while(count<150)
-		count++;
+	/*sirve para almacenar temporalmente los valores de los registros que contienen
+	 *datos de 16bits
+	 */
+	int16_t data_word;
+
+	/*sirve para almacenar temporalmente los valores de los registros que contienen
+	 *datos de 8bits
+	 */
+
+	uint8_t data_byte;
+	/*sirve para almacenar temporalmente las direcciones a pasar como parametro de los
+	 * registros que deberan ser leidos
+	 */
+	uint8_t regAdress;
+
+	/*calcular offset del registro x del acelerometro*/
 	regAdress=MPU6050_RA_ACCEL_XOUT_H;
 	MPU_6050_readReg(fd_i2c,regAdress,&data_byte);
 	data_word=(int16_t)((data_byte<<8)&0xff00);
 	++regAdress;
 	MPU_6050_readReg(fd_i2c,regAdress,&data_byte);
 	data_word=(data_word | data_byte);
-	x_accel_offset=-data_word;
+	MPU_data_offset.xacc=-data_word;
+
+	/*calcular offset del registro y del acelerometro*/
 	regAdress=MPU6050_RA_ACCEL_YOUT_H;
 	MPU_6050_readReg(fd_i2c,regAdress,&data_byte);
 	data_word=(int16_t)((data_byte<<8)&0xff00);
 	++regAdress;
 	MPU_6050_readReg(fd_i2c,regAdress,&data_byte);
 	data_word=(data_word | data_byte);
-	y_accel_offset=-data_word;
+	MPU_data_offset.yacc=-data_word;
+
+	/*calcular offset del registro z del acelerometro*/
 	regAdress=MPU6050_RA_ACCEL_ZOUT_H;
 	MPU_6050_readReg(fd_i2c,regAdress,&data_byte);
 	data_word=(int16_t)((data_byte<<8)&0xff00);
 	++regAdress;
 	MPU_6050_readReg(fd_i2c,regAdress,&data_byte);
 	data_word=(data_word | data_byte);
-	z_accel_offset=((float)INT16_MAX/AFS_SEL)-data_word;
-	//
+	MPU_data_offset.zacc=((float)INT16_MAX/AFS_SEL)-data_word;
+
+	/*calcular offset del registro x del giroscopio*/
 	regAdress=MPU6050_RA_GYRO_XOUT_H;
 	MPU_6050_readReg(fd_i2c,regAdress,&data_byte);
 	data_word=(int16_t)((data_byte<<8)&0xff00);
 	++regAdress;
 	MPU_6050_readReg(fd_i2c,regAdress,&data_byte);
 	data_word=(data_word | data_byte);
-	x_gyro_offset=-data_word;
+	MPU_data_offset.xgyro=-data_word;
+
+	/*calcular offset del registro y del giroscopio*/
 	regAdress=MPU6050_RA_GYRO_YOUT_H;
 	MPU_6050_readReg(fd_i2c,regAdress,&data_byte);
 	data_word=(int16_t)((data_byte<<8)&0xff00);
 	++regAdress;
 	MPU_6050_readReg(fd_i2c,regAdress,&data_byte);
 	data_word=(data_word | data_byte);
-	y_gyro_offset=-data_word;
+	MPU_data_offset.ygyro=-data_word;
+
+	/*calcular offset del registro z del giroscopio*/
 	regAdress=MPU6050_RA_GYRO_ZOUT_H;
 	MPU_6050_readReg(fd_i2c,regAdress,&data_byte);
 	data_word=(int16_t)((data_byte<<8)&0xff00);
 	++regAdress;
 	MPU_6050_readReg(fd_i2c,regAdress,&data_byte);
 	data_word=(data_word | data_byte);
-	z_gyro_offset=-data_word;
-	return UINT8_MAX;
+	MPU_data_offset.zgyro=-data_word;
+	return 1;
 }
 
 uint8_t  MPU_6050_setup(int32_t	fd_i2c,uint16 tick){
@@ -224,10 +238,11 @@ uint8_t  MPU_6050_setup(int32_t	fd_i2c,uint16 tick){
 	//la siguiente linea es para seleccionar la escala del gyroscopio +-250º/s
 	//MPU_6050_writeReg(fd_i2c,MPU6050_RA_GYRO_CONFIG,0);
 
-	//filtro
+	//TODO ver si la siguiente es para habilitar el filtro
 	MPU_6050_writeReg(fd_i2c,MPU6050_RA_CONFIG,6);
 
 	/*
+	 * TODO esta funcion deberia llamarse luego de 30 ms
 	 * Calibrate sensor
 	 */
 	MPU_6050_calibrate(fd_i2c);
@@ -235,25 +250,5 @@ uint8_t  MPU_6050_setup(int32_t	fd_i2c,uint16 tick){
 	/*
 	 * Compruebo que el sensor funciona correctamente
 	 */
-	if(MPU_6050_test(fd_i2c)==1)
-		return 1;
-	return 0;
+	return 1;
 }
-
-
-//
-//	/*
-//	 * Para probar si hay esclavos...
-//	 * PROBAR ESCLAVOS
-//	 */
-//	//      		int i;
-//	//      			uint8_t ch;
-//	//
-//	//      			for (i = 0; i <= 0x7F; i++) {
-//	//
-//	//      				if (Chip_I2C_MasterRead(I2C0, i, &ch, 1 ) > 0) {
-//	//      					ciaaPOSIX_printf("NO anda!!!\n");//Aca poner un breakpoint y ver cuando para que valor tiene ch
-//	//      				}
-//	//
-//	//      			}
-//	//
--- Software/src/Main.c
+++ Software/src/Main.c
@@ -1,62 +1,4 @@
-/* Copyright 2015, Marcelo Minig Traverso
- * Copyright 2015, Manuel Garcia
- *
- * This file is part of CIAA Firmware.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- *
- * 3. Neither the name of the copyright holder nor the names of its
- *    contributors may be used to endorse or promote products derived from this
- *    software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- *
- */
-
-/** \brief i2c_MPU6050_Slave example source file
- **
- ** This is a mini example of the CIAA Firmware for use with a MPU-6050™ MotionTracking™ device
- **
- **
- **/
-
-/** \addtogroup CIAA_Firmware CIAA Firmware
- ** @{ */
-/** \addtogroup Examples CIAA Firmware Examples
- ** @{ */
-/** \addtogroup i2c_MPU6050_Slave example source file
- ** @{ */
-
-/*
- * Initials     Name
- * ---------------------------
- * MaMT         Marcelo Minig Traverso
- * MaGa         Manuel Garcia
- */
-
-/*
- * modification history (new versions first)
- * -----------------------------------------------------------
- * 20150921 v0.0.1   MaGa   first functional version
- *//*==================[inclusions]=============================================*/
+/*==================[inclusions]=============================================*/
 #include "../inc/Main.h"    /* <= own header */
 
 #include "os.h"               /* <= operating system header */
@@ -75,41 +17,47 @@
 #define TEC2_MASK         0B0010
 #define TEC3_MASK         0B0100
 #define TEC4_MASK         0B1000
-#define LED_GREEN				(0x20)
-#define LED_RED					(0x10)
-#define LED_ORANGE				(0x08)
-#define LED_BLUE_RGB			(0x04)
-#define LED_GREEN_RGB			(0x02)
-#define LED_RED_RGB				(0x01)
-#define TEMP_GREA_30C			LED_RED
-#define TEMP_LESS_20C_GREA_30C	LED_ORANGE
-#define TEMP_LESS_20C			LED_BLUE_RGB
-#define tick					10
-#define xanglealarm				45
-#define yanglealarm				45
-#define xaccalarm				10
-#define yaccalarm				10
+#define LEDS_RESET		  0B11000000
+#define TICK			  10
+#define X_ANGLEA_LARM	  45
+#define Y_ANGLE_ALARM	  45
+#define X_ACC_ALARM		  10
+#define Y_ACC_ALARM		  10
 
 
 
 /*==================[internal data declaration]==============================*/
 
-/*en estas variables guardo las lecturas del MPU. tambien se guardan en la libreria del
- * mpu, decidir que conviene hacer
- */
-float xgyro=0,ygyro=0,zgyro=0,xacc=0,yacc=0,zacc=0;
+/* en esta estructura guardo las lecturas del MPU*/
+MPU_data_struct MPU_data;
 
-/* en num guardo el valor de los registros del giroscopio convertidos a string*/
+/* TODO reemplazar o reutilizar para cuando se implemente el telit
+ * en num guardo el valor de los registros del giroscopio convertidos a string
+ */
 char num[]="    ";
 
-/*en message combino todos los strings generados en la lectura de los ejes x,y,z*/
+/*TODO idem anterior. en message combino todos los strings generados en la lectura
+ * de los ejes x,y,z
+ */
 char message[80];
 
+/*luces*/
 uint8 outputs;
 
+/*determina el estado de emergencia*/
 char alarma=0;
 
-char countSerial=0,countAlarm=0;
+/*TODO esta variable debera ser eliminada cuando se implemente la comunicacion
+ * serie con el telit, ya que se usa para determinar cada cuanto tiempo se deberan
+ * mostrar  por terminal los valores
+ */
+char countSerial=0;
+
+/*Se utiliza para llevar la cuenta regresiva ants de mandar el sms*/
+char countAlarm=0;
+
+
+
 
 /*==================[internal functions declaration]=========================*/
 
@@ -200,9 +148,6 @@ TASK(InitTask)
 	/* init CIAA kernel and devices */
 	ciaak_start();
 
-	/* print message (only on x86) */
-	ciaaPOSIX_printf("Init Task...\n");
-
 	/* open UART connected to USB bridge (FT2232) */
 	fd_uart1 = ciaaPOSIX_open("/dev/serial/uart/1", O_RDWR);
 
@@ -212,8 +157,14 @@ TASK(InitTask)
 	/* change FIFO TRIGGER LEVEL for uart usb */
 	ciaaPOSIX_ioctl(fd_uart1, ciaaPOSIX_IOCTL_SET_FIFO_TRIGGER_LEVEL, (void *)ciaaFIFO_TRIGGER_LEVEL3);
 
-	/*Abro la comunicacion i2c con el sensor:*/
-	fd_i2c = MPU_6050_open(ciaaPOSIX_IOCTL_CLOCKRATE_400000);
+	/* Abro la comunicacion i2c (I2C0)*/
+	fd_i2c = ciaaPOSIX_open("/dev/i2c/0", O_RDWR);
+
+	/* seteamos el clockrate*/
+	ciaaPOSIX_ioctl(fd_i2c, ciaaPOSIX_IOCTL_SET_CLOCKRATE, (void *)ciaaPOSIX_IOCTL_CLOCKRATE_400000);
+
+	/* Configuramos la direccion del esclavo*/
+	ciaaPOSIX_ioctl(fd_i2c, ciaaPOSIX_IOCTL_SET_SLAVEADD, (void *)MPU6050_ADDRESS);
 
 	/* open CIAA digital outputs */
 	fd_out = ciaaPOSIX_open("/dev/dio/out/0", O_RDWR);
@@ -221,14 +172,17 @@ TASK(InitTask)
 	/* open CIAA digital inputs */
 	fd_in = ciaaPOSIX_open("/dev/dio/in/0", O_RDWR);
 
-	/*Setup MPU*/
-	MPU_6050_setup(fd_i2c,tick);
+	/*se realizan las configuraciones iniciales del MPU*/
+	MPU_6050_setup(fd_i2c,TICK);
+
+	if(!MPU_6050_test(fd_i2c))
+		ErrorHook();
 
 	/* activate periodic task:
 	 *  - for the first time after 350 ticks (350 ms)
-	 *  - and then every 10 ticks (10 ms)
+	 *  - and then every tick ticks (definido como macro)
 	 */
-	SetRelAlarm(ActivatePeriodicTask, 350, tick);
+	SetRelAlarm(ActivatePeriodicTask, 350, TICK);
 
 	/* terminate task */
 	TerminateTask();
@@ -242,41 +196,51 @@ TASK(InitTask)
  */
 TASK(PeriodicTask)
 {
-	/*cargo los angulos de posicion y las aceleraciones*/
-	MPU_6050_readData(fd_i2c,&xgyro,&ygyro,&zgyro,&xacc,&yacc,&zacc);
+	/* actualizo los angulos de posicion y las aceleraciones*/
+	MPU_6050_update(fd_i2c,&MPU_data);
 
-	/* Envio el mensaje completo a transmitir por serie cada 1 seg*/
-	if(++countSerial>(1000/tick)){
+	/* Envio el mensaje completo a transmitir por serie cada 1 seg
+	 * Esto se usa para ver los valores por la terminal, debera ser eliminado
+	 * cuando se implemente el codigo para enviar el sms*/
+	if(++countSerial>(1000/TICK)){
 		/* Activates the SerialEchoTask task */
 		ActivateTask(SerialEchoTask);
 		countSerial=0;
 	}
 
-	/*activacion de la alarma si se supera en cualquier eje los 45º*/
-	if((((abs(xgyro)>xanglealarm)|(abs(ygyro)>yanglealarm))|((xacc<-xaccalarm)|(abs(yacc)>yaccalarm)))&&alarma!=1){
+	/* activacion de la alarma si se supera (en modulo) en los ejes 'x' o 'y' a los
+	 * valores maximos en cualquier sentido, si la aceleracion lateral supera (en modulo)
+	 * los valores maximos o si se produce una desaceleracion brusca en el eje y. ademas
+	 * se inicia countAlarm para realizar la cuenta regresiva antes de mandar el mensaje
+	 */
+	if((((abs(MPU_data.xgyro)>X_ANGLEA_LARM)|(abs(MPU_data.ygyro)>Y_ANGLE_ALARM))|((MPU_data.yacc<-Y_ACC_ALARM)|(abs(MPU_data.xacc)>X_ACC_ALARM)))&&alarma!=1){
 		alarma=1;
-		//para las luces
-		outputs=0b00000111;
 		countAlarm=0;
 	}
 
 	/*
-	 * Esto prende y apaga las luces,pero aca se deberia llamar a la funcion
-	 * que envia el mensaje de ayuda. se deberia implementar un contador para dar tiempo
-	 * a cancelar en caso de que se dispare la alerta de forma erronea
+	 * control de las luces y de la cuenta regresiva para mandar el mensaje
 	 */
 	if(alarma==0){
-		outputs =0x00;
-		ciaaPOSIX_write(fd_out, &outputs, 1);
+		outputs =LEDS_RESET;
 	}else{
-		if((++countAlarm>(1000/tick)) & (outputs!= 0x3f)){
-			outputs=outputs | (outputs << 1 ) ;
+		if((++countAlarm>(1000/TICK)) & (outputs!= 0x3f)){
+			outputs=outputs | (outputs >> 1 ) ;
 			countAlarm=0;
 		}
-		//ciaaPOSIX_read(fd_out, &outputs, 1);
-		//outputs ^= 0x3f;
-		ciaaPOSIX_write(fd_out, &outputs, 1);
+		/* si se cumple este if, se llego al final de la cuenta y se debe mandar
+		 * el mensaje de auxilio*/
+		if(outputs== 0x3f){
+			/*
+			 * Aca debo configurar el mensaje a enviar por sms, y luego activar la
+			 * serial task
+			 */
+
+			/* Activates the SerialEchoTask task */
+					//ActivateTask(SerialEchoTask);
+		}
 	}
+	ciaaPOSIX_write(fd_out, &outputs, 1);
 	TerminateTask();
 }
 
@@ -305,7 +269,7 @@ TASK(KeysTask)
 	if (TEC1_MASK & edgeUpKeys)
 	{
 		alarma=0;
-		outputs =0x00;
+		outputs =LEDS_RESET;
 		ciaaPOSIX_write(fd_out, &outputs, 1);
 	}
 
@@ -313,10 +277,7 @@ TASK(KeysTask)
 	if (TEC2_MASK & edgeUpKeys)
 	{
 		alarma=1;
-		//para las luces
-		outputs=0b00000111;
 		countAlarm=0;
-
 	}
 
 	/*
@@ -341,32 +302,32 @@ TASK(KeysTask)
 
 
 /*
- * Esto se usa para debuguear y ver los valores. quizas se deberia implementar aca la
+ * Esto se usa para debuguear y ver los valores. se deberia implementar aca la
  * comunicacion serie con el modulo telit
  */
 TASK(SerialEchoTask)
 {
-	itoa((int)xgyro, num, 10);
+	itoa((int)MPU_data.xgyro, num, 10);
 	strcpy(message,"X=");
 	strcat(message,num);
 	strcat(message,"º\r\n");
-	itoa((int)ygyro, num, 10);
+	itoa((int)MPU_data.ygyro, num, 10);
 	strcat(message,"Y=");
 	strcat(message,num);
 	strcat(message,"º\r\n");
-	itoa((int)zgyro, num, 10);
+	itoa((int)MPU_data.zgyro, num, 10);
 	strcat(message,"Z=");
 	strcat(message,num);
 	strcat(message,"º\r\n");
-	itoa((int)xacc, num, 10);
+	itoa((int)MPU_data.xacc, num, 10);
 	strcat(message,"X=");
 	strcat(message,num);
 	strcat(message,"m/s\r\n");
-	itoa((int)yacc, num, 10);
+	itoa((int)MPU_data.yacc, num, 10);
 	strcat(message,"Y=");
 	strcat(message,num);
 	strcat(message,"m/s\r\n");
-	itoa((int)zacc, num, 10);
+	itoa((int)MPU_data.zacc, num, 10);
 	strcat(message,"Z=");
 	strcat(message,num);
 	strcat(message,"m/s\r\n");
@@ -377,16 +338,7 @@ TASK(SerialEchoTask)
 
 /*
  * PROBLEMAS
- * --Se debe hacer el filtro para las medidas del giroscopio.
- * --la calibracion deberia hacerse despues de 30 ms(pag * 12 del datashet)
- * --la alarma deberia llamarse mediante una funcion y quedar registrada en una variable,
- *    no depender directamente de los valores del gyroscopio/acelerometro
- *-- se deben quitar variables y codigo redundante
- *-- el boton de panico y el de cancelar deberian trabajar sobre la variable
- *   alarma/funcion no implementada todavia
- *-- preguntar como se deben tratar las pequeñas variaciones-> creo q el dt q estoy
- *   tomando no es correcto:yo toy tomando los tick del osek, pero capaz q hay q
- *   hacerlo con el samplerate del mpu
+ * -- la calibracion deberia hacerse despues de 30 ms(pag * 12 del datashet)
  * -- el boton cancelar deberia llamar a una recalibracion? si cancelo con los angulos
  * de posicion superancdo los limites se va a volver a llamar la alarma
  */
